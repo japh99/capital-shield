cb25d89b3c7abf55a19d23ebf3635eed512712f6
JAPH99 <jampo568@gmail.com>
2026-01-04 19:55:15 -0500

    Update __init__.py

--- api/engines/__init__.py
+++ api/engines/__init__.py
@@ -1,1 +1,5 @@
+from .soccer import calculate_expected_goals as soccer_engine
+from .nba import calculate_expected_margin as nba_engine
+from .mlb import calculate_expected_runs as mlb_engine
 
+__all__ = ['soccer_engine', 'nba_engine', 'mlb_engine']
